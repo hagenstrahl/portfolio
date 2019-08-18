7e03d40ee19e325aba6ea3d4c4a4885d303d3748
Hagen Strahl <hagen.strahl@googlemail.com>
2019-08-18 08:46:28 +0200

    commit before branch

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function toogleNavigation(menuIcon) {
-    menuIcon.classList.toggle("change");
-    var topNav = document.getElementById("nav-bar");
-    if (topNav.className === "topnav") {
-        topNav.className += " responsive";
-    } else {
-        topNav.className = "topnav";
-    }
-}
